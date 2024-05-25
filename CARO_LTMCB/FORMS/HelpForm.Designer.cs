a389a0e63d0cd08f406912963ce93d60db915c9a
TuUyen12 <lethituuyen1204@gmail.com>
2024-05-26 00:32:36 +0700

    Them form Helpform

--- CARO_LTMCB/FORMS/HelpForm.Designer.cs
+++ CARO_LTMCB/FORMS/HelpForm.Designer.cs
@@ -29,20 +29,130 @@ namespace CARO_LTMCB.FORMS
         /// </summary>
         private void InitializeComponent()
         {
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(674, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 29);
+            this.label6.TabIndex = 79;
+            this.label6.Text = "ID: ";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtID.Location = new System.Drawing.Point(751, 109);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(168, 38);
+            this.txtID.TabIndex = 78;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(280, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 29);
+            this.label3.TabIndex = 77;
+            this.label3.Text = "Username: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtName.Location = new System.Drawing.Point(418, 109);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 38);
+            this.txtName.TabIndex = 76;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.richTextBox1.Location = new System.Drawing.Point(69, 288);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(850, 328);
+            this.richTextBox1.TabIndex = 71;
+            this.richTextBox1.Text = "";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtMail.Location = new System.Drawing.Point(418, 171);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(501, 38);
+            this.txtMail.TabIndex = 70;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(280, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 69;
+            this.label1.Text = "Gmail: ";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSend.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnSend.FlatAppearance.BorderSize = 4;
+            this.btnSend.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSend.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnSend.Location = new System.Drawing.Point(69, 72);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(168, 151);
+            this.btnSend.TabIndex = 68;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.UseWaitCursor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
             this.ClientSize = new System.Drawing.Size(1020, 745);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HelpForm";
             this.Text = "HelpForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
     }
 }
--- CARO_LTMCB/FORMS/HelpForm.cs
+++ CARO_LTMCB/FORMS/HelpForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 using System.Windows.Forms;
 
 namespace CARO_LTMCB.FORMS
@@ -15,6 +10,40 @@ namespace CARO_LTMCB.FORMS
         public HelpForm()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string ID = txtID.Text;
+            string email = txtMail.Text;
+            string password = "gkbx ggdt nguk gxdk";
+            string message = richTextBox1.Text;
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(email);
+                mail.To.Add("carogame2024@gmail.com");
+                mail.Subject = "Thắc mắc từ người chơi: " + name + "-" + ID;
+                mail.Body = $"Email: {email}\nNội dung:\n{message}";
+
+                smtpServer.Port = 587; // hoặc 465 tùy thuộc vào nhà cung cấp email
+                smtpServer.Credentials = new NetworkCredential("usercarogame@gmail.com", password);
+                smtpServer.EnableSsl = true;
+
+                smtpServer.Send(mail);
+                MessageBox.Show("Tin nhắn của bạn đã được gửi thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi nếu việc gửi email thất bại
+                MessageBox.Show("Có lỗi xảy ra khi gửi tin nhắn: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
     }
 }
